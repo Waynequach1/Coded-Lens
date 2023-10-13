30e68cec8e581c8851c30310c7940a878be4faca
Waynequach1 <waynequach1@gmail.com>
2023-10-13 16:32:24 -0400

    Add buttons for inversion and navigation

--- src/app/get-started/lens-at-work/lens-at-work.component.ts
+++ src/app/get-started/lens-at-work/lens-at-work.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./lens-at-work.component.css']
 })
 export class LensAtWorkComponent {
+  public goodSeverity = 'success'
+  public goodText = 'The Good'
+  public badSeverity = 'danger'
+  public badText = 'The Bad'
 
+  // Inverts the button displays
+  public handleOnHover(isGood: boolean) {
+    if (isGood) {
+      this.goodSeverity = 'danger';
+      this.goodText = 'The Bad';
+    } else {
+      this.badSeverity = 'success';
+      this.badText = 'The Good';
+    }
+  }
+
+  // Reverts the inversion
+  public handleOnLeave(isGood: boolean) {
+    if (isGood) {
+      this.goodSeverity = 'success';
+      this.goodText = 'The Good';
+    } else {
+      this.badSeverity = 'danger';
+      this.badText = 'The Bad';
+    }
+  }
 }
